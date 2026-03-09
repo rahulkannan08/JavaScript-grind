32cd97413524acd7507ab6950a63400e7580b275
rahulkannan08 <rahul08supermacy@gmail.com>
2026-03-09 23:05:01 +0530

    updated

--- FSD/pg10/form.js
+++ FSD/pg10/form.js
@@ -25,7 +25,7 @@ app.get("/", async (req, res) => {
     output += `
       <p>
         ${s.name} | ${s.age} | ${s.course}
-        <a href="/delete/${s._id}">Delete</a>
+        <a href="/delete/${s._id}">Delete</a> <a href="/update/${s._id}">Update</a>
       </p>
     `; });
   res.send(output);});
@@ -35,3 +35,30 @@ app.post("/add", async (req, res) => {
 app.get("/delete/:id", async (req, res) => {
   await Student.findByIdAndDelete(req.params.id);
   res.redirect("/");});
+app.get("/update/:id", async (req, res) => {
+  const student = await Student.findById(req.params.id);
+  res.send(`
+    <h1>Update Student</h1> 
+    <form action="/update/${student._id}" method="POST">
+      Name: <input type="text" name="name" value="${student.name}" required><br><br>
+      Age: <input type="number" name="age" value="${student.age}" required><br><br>
+      Course: <input type="text" name="course" value="${student.course}" required><br><br>
+      <button type="submit">Update Student</button>
+    </form>
+  `);});
+app.post("/update/:id", async (req, res) => {
+  await Student.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect("/");});
+app.get("/edit/:id", async (req, res) => {
+  const student = await Student.findById(req.params.id);
+  res.send(`
+    <h1>Edit Student</h1>
+    <form action="/update/${student._id}" method="POST">
+      Name: <input type="text" name="name" value="${student.name}" required><br><br>
+      Age: <input type="number" name="age" value="${student.age}" required><br><br>
+      Course: <input type="text" name="course" value="${student.course}" required><br><br>
+      <button type="submit">Update Student</button>
+    </form>
+  `);});
+
+app.listen(3000, () => console.log("Server running on port 3000")); 
