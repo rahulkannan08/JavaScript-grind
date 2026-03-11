cc0add2dcdbb2e731638457bde977280947c148e
rahulkannan08 <rahul08supermacy@gmail.com>
2026-03-11 21:57:06 +0530

    solved some errors

--- FSD/pg10/form.js
+++ FSD/pg10/form.js
@@ -1,61 +1,53 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
-
+const fs = require("fs");
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public")); // serve HTML from public folder
-
-// MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/studentDB")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
-// Schema
-const studentSchema = new mongoose.Schema({
+.then(() => console.log("MongoDB Connected"));
+const Student = mongoose.model("Student", {
   name: String,
   age: Number,
   course: String
 });
-
-const Student = mongoose.model("Student", studentSchema);
-
-
-// Get all students
-app.get("/students", async (req, res) => {
+app.get("/", async (req, res) => {
   const students = await Student.find();
-  res.json(students);
+  let html = fs.readFileSync("./public/index.html", "utf8");
+  let list = "<h2>Student List</h2><ul>";
+  students.forEach(s => {
+    list += `<li>
+      ${s.name} - ${s.age} - ${s.course}
+      <a href="/edit/${s._id}">Update</a>
+      <a href="/delete/${s._id}">Delete</a>
+    </li>`;
+  });
+  list += "</ul>";
+  res.send(html + list);
 });
-
-
-// Add student
 app.post("/add", async (req, res) => {
   await Student.create(req.body);
   res.redirect("/");
 });
-
-
-// Delete student
 app.get("/delete/:id", async (req, res) => {
   await Student.findByIdAndDelete(req.params.id);
   res.redirect("/");
 });
-
-
-// Get student for edit
 app.get("/edit/:id", async (req, res) => {
-  const student = await Student.findById(req.params.id);
-  res.json(student);
+  const s = await Student.findById(req.params.id);
+  res.send(`
+    <h2>Update Student</h2>
+    <form method="POST" action="/update/${s._id}">
+    Name: <input type="text" name="name" value="${s.name}"><br><br>
+    Age: <input type="number" name="age" value="${s.age}"><br><br>
+    Course: <input type="text" name="course" value="${s.course}"><br><br>
+    <button type="submit">Update</button>
+    </form>
+  `);
 });
-
-
-// Update student
 app.post("/update/:id", async (req, res) => {
   await Student.findByIdAndUpdate(req.params.id, req.body);
   res.redirect("/");
 });
-
-// Server start
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
